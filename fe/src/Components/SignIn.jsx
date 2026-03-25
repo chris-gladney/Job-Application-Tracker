4d8ccd4a0ac4d1e836e686b8b83eeeb541fb280d
Christopher Gladney <cgladney@mba-group.com>
2026-03-25 14:53:15 +0000

    Login form styling added

--- fe/src/Components/SignIn.jsx
+++ fe/src/Components/SignIn.jsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom";
 function SignIn() {
   return (
     <>
-      <form>
+      <form className="sign-in">
         <h3>Login</h3>
-        <input type="text" placeholder="Email Address" />
-        <br />
+        <input type="email" placeholder="Email Address" />
+
         <input type="password" placeholder="Password" />
-        <br />
+
         <div className="checked">
           <input type="checkbox" name="remember-me" id="remember-me" />
           <label htmlFor="remember-me">Remember me</label>
         </div>
         <button className="orange-button">Login</button>
-        <br />
+
         <button className="forgot-password">Forgot Password?</button>
-        <br />
+
         <p>
           Don't have an account? <Link to="/register">Sign Up</Link>
         </p>
