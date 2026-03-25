e2e5bef379d68ba321a998279361f899a2949dee
Christopher Gladney <cgladney@mba-group.com>
2026-03-25 17:21:55 +0000

    Main page initialised

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import SignIn from './Components/SignIn'
-import SignUp from './Components/SignUp'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import JobForm from './Components/JobForm'
+import { useState } from "react";
+import "./App.css";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import JobForm from "./Components/JobForm";
+import JobsBoard from "./Components/JobsBoard";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
 
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<JobForm />} />
-      <Route path="/register" element={<SignUp />} />
-    </Routes>
-  </BrowserRouter>
+        {/* Routes below this point will be protected */}
+        <Route path="/add-job" element={<JobForm />} />
+        <Route path="/jobs-board" element={<JobsBoard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
